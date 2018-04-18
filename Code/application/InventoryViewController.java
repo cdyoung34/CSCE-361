52e3353a80c76e6ae51b601ca05a2a6dd5097a54
cdyoung34 <37003351+cdyoung34@users.noreply.github.com>
2018-04-18 04:56:51 -0500

    Add files via upload

--- Code/application/InventoryViewController.java
+++ Code/application/InventoryViewController.java
@@ -136,7 +136,15 @@ public class InventoryViewController implements Initializable {
 		window.show();
 	}
 	public void toStatistic(ActionEvent event) throws IOException{
+		Parent statisticsViewParent = FXMLLoader.load(getClass().getResource("StatisticsView.fxml"));
+		Scene statisticsScene = new Scene(statisticsViewParent,680,470);
 		
+		// get the stage information
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		
+		window.setScene(statisticsScene);
+		window.setResizable(true);
+		window.show();
 	}
 	
 	/**
