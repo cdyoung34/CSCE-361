5738d60123771c61ef79dbdca86f92450aa8323d
cdyoung34 <37003351+cdyoung34@users.noreply.github.com>
2018-04-19 16:08:01 -0500

    Add files via upload
    EditView doesn't allow invalid inputs where numbers should be. it give a popup

--- Code/application/EditViewController.java
+++ Code/application/EditViewController.java
@@ -4,12 +4,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
-import java.util.function.Predicate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
-import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -20,20 +17,16 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import rim.Cart;
 import rim.Product;
 
 public class EditViewController implements Initializable{
 
-	
 	@FXML private ListView thresholdList;
+	@FXML private ListView listView;
 	@FXML private TextField productName;
 	@FXML private TextField type;
 	@FXML private TextField supplier;
@@ -42,63 +35,13 @@ public class EditViewController implements Initializable{
 	@FXML private TextField threshold;
 	@FXML private TextArea description;
 	@FXML private Label label;
-	@FXML private TableView inventoryTable;
-	@FXML private TableColumn nameColumn;
-	@FXML private TableColumn priceColumn;
-	@FXML private TableColumn stockColumn;
-	@FXML private TextField searchField;
 	private Boolean updateMode = false;
 	private Boolean addMode = false;
 	
-	// for search bar
-	ObservableList os = FXCollections.observableArrayList();
-	FilteredList filter = new FilteredList(os, e->true);
-	
-	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		this.setInventoryTable();
-		inventoryTable.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-		
+		this.setInventoryList();
 		this.setThresholdList();
-		
-		nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-		priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
-		stockColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
-		
-		
-		
-	}
-	
-	private void setInventoryTable() {
-		List<Product> products = Product.getProducts();
-		for(Product p : products) {
-			inventoryTable.getItems().add(p);
-			os.add((Product) p);
-		}
-	}
-	
-	/**
-	 * filters search bar
-	 * @param event
-	 */
-	public void search(KeyEvent event) {
-		searchField.textProperty().addListener((observable, oldValue, newValue) -> {
-			filter.setPredicate((Predicate<Product>) (Product product)->{
-			
-			if (newValue.isEmpty() || newValue == null) {
-				return true;
-			} else if (product.getName().toUpperCase().contains(newValue.toUpperCase())) {
-				return true;
-			}
-			return false;
-			
-			});
-		});
-		
-		SortedList sort = new SortedList(filter);
-		sort.comparatorProperty().bind(inventoryTable.comparatorProperty());
-		inventoryTable.setItems(sort);
 	}
 	
 	/**
@@ -106,7 +49,7 @@ public class EditViewController implements Initializable{
 	 */
 	public void returnToLogin(ActionEvent event) throws IOException{
 		Parent loginViewParent = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
-		Scene loginViewScene = new Scene(loginViewParent, 480, 320);
+		Scene loginViewScene = new Scene(loginViewParent);
 		
 		// get the stage information
 		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
@@ -120,10 +63,12 @@ public class EditViewController implements Initializable{
 	 * remove item from inventory
 	 */
 	public void removeButtonPressed() {
-		Product p = (Product) inventoryTable.getSelectionModel().getSelectedItem();
-		inventoryTable.getItems().remove(inventoryTable.getSelectionModel().getSelectedIndex());
+		Product p = findProduct((String) listView.getSelectionModel().getSelectedItem());
+		listView.getItems().remove(listView.getSelectionModel().getSelectedIndex());
 		Product.removeProduct(p);
+		this.setThresholdList();
 		this.clearTextFields();
+		label.setText("Update Inventory");
 	}
 
 	public void newButtonPressed() {
@@ -135,21 +80,37 @@ public class EditViewController implements Initializable{
 	
 	/**
 	 * Save edits made, whether it be adding or updating an item
+	 * @throws IOException 
 	 */
-	public void saveButtonPressed() {
-		String pName = productName.getText();
+	public void saveButtonPressed(ActionEvent event) throws IOException {
+		String pName = "";
+		if(Product.nameExists(productName.getText()) && addMode) {
+			triggerPopup(event, "The name you inputted already exists");
+			return;
+		} else {
+			pName = this.productName.getText();
+		}
 		String pType = type.getText();
 		String pSup = supplier.getText();
-		double pPrice = Double.parseDouble(price.getText());
-		int pQuan = Integer.parseInt(quantity.getText());
-		int pThre = Integer.parseInt(threshold.getText());
+		double pPrice = 0;
+		int pQuan = 0;
+		int pThre = 0;
 		String pDes = description.getText();
+		try {
+			pPrice = Double.valueOf(this.price.getText());
+			pQuan = Integer.valueOf(this.quantity.getText());
+			pThre = Integer.valueOf(this.threshold.getText());
+		} catch(NumberFormatException nfe) {
+			triggerPopup(event, null);
+			return;
+		}
+
+		
 		if (this.addMode) {
 			Product.addProduct(pName,pType,pDes,pQuan,pThre,pPrice,pSup);
-			this.setInventoryTable();
+			listView.getItems().add(pName);
 		} else if (this.updateMode) {
-			// TODO: finish functionality for updating an item
-			Product product = (Product) inventoryTable.getSelectionModel().getSelectedItem();
+			Product product = findProduct((String) listView.getSelectionModel().getSelectedItem());
 			Product.updatePrice(product, pPrice);
 			Product.updateQuantity(product.getId(), pQuan);
 			Product.updateDescription(product, pDes);
@@ -158,31 +119,19 @@ public class EditViewController implements Initializable{
 			Product.updateThreshold(product, pThre);
 			Product.updateType(product, pType);
 		}
-		
-		this.thresholdList.getItems().clear();
-		this.setThresholdList();		
+		// clear text fields
 		this.clearTextFields();
+		// default label
 		label.setText("Update Inventory");
+		// refresh threshold list
+		this.setThresholdList();
+		// refresh list view
+		this.setInventoryList();
 	}
 	
-//	public void addButtonPressed() {
-//		String pName = productName.getText();
-//		String pType = type.getText();
-//		String pSup = supplier.getText();
-//		double pPrice = Double.parseDouble(price.getText());
-//		int pQuan = Integer.parseInt(quantity.getText());
-//		int pThre = Integer.parseInt(threshold.getText());
-//		String pDes = description.getText();
-//		
-//		Product.addProduct(pName,pType,pDes,pQuan,pThre,pPrice,pSup);
-//		listView.getItems().add(pName);
-//		
-//		this.clearTextFields();
-//	}
-//	
 	public void returnToInventory(ActionEvent event) throws IOException {
 		Parent inventoryViewParent = FXMLLoader.load(getClass().getResource("InventoryView.fxml"));
-		Scene inventoryViewScene = new Scene(inventoryViewParent, 1000, 600);
+		Scene inventoryViewScene = new Scene(inventoryViewParent,1200, 800);
 		
 		// get the stage information
 		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
@@ -196,15 +145,21 @@ public class EditViewController implements Initializable{
 	 * Update Item Button, this method sets the mode but does not actually update the item
 	 */
 	public void updateButtonPressed() {
-		label.setText("Update Item: " + ((Product) inventoryTable.getSelectionModel().getSelectedItem()).getName());
+		label.setText("Update Item: " + listView.getSelectionModel().getSelectedItem());
 		
-		Product p = (Product) inventoryTable.getSelectionModel().getSelectedItem();
+		Product p = findProduct((String) listView.getSelectionModel().getSelectedItem());
 		productName.setText(p.getName());
+		//System.out.println("Name: " +p.getName());
 		type.setText(p.getType());
+		//System.out.println("Type: " + p.getType());
 		supplier.setText(p.getSupplier());
+		//System.out.println("Supplier: " + p.getSupplier());
 		quantity.setText("" + p.getQuantity());
+		//System.out.println("Price: " + p.getPrice());
 		price.setText("" + p.getPrice());
+		//System.out.println("Description: " + p.getDescription());
 		description.setText(p.getDescription());
+		//System.out.println("Threshold: " + p.getThreshold());
 		threshold.setText("" + p.getThreshold());
 		
 		this.updateMode = true;
@@ -234,9 +189,45 @@ public class EditViewController implements Initializable{
 	}
 	
 	private void setThresholdList() {
+		this.thresholdList.getItems().clear();
 		List<Product> thresholdItems = Product.getThresholdProducts();
 		for (Product p : thresholdItems) {
 			thresholdList.getItems().add(p.getName());
 		}
+	}
+	
+	private void setInventoryList() {
+		this.listView.getItems().clear();
+		List<Product> products = Product.getProducts();
+		for(Product p : products) {
+			listView.getItems().add(p.getName());
+		}
+		listView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+	}
+	
+	public  void triggerPopup(ActionEvent event, String message) throws IOException {
+		
+		// set popup message
+		FXMLLoader Loader = new FXMLLoader();
+		Loader.setLocation(getClass().getResource("InvalidInputPopup.fxml"));
+		try {
+			Loader.load();
+		} catch (IOException ex) {
+			Logger.getLogger(managerViewController.class.getName()).log(Level.SEVERE, null, ex);
+			System.out.println("Nope");
+		}
+
+		InvalidInputPopupController popupController = Loader.getController();
+		popupController.setMessage(message);
+		
+		// link to popup screne
+		Parent invalidCreds = FXMLLoader.load(getClass().getResource("InvalidInputPopup.fxml"));
+		Scene invalidCredScene = new Scene(invalidCreds,400, 200);
+		
+		Stage window = new Stage();
+		
+		window.setScene(invalidCredScene);		
+		window.setResizable(false);
+		window.showAndWait();
 	}
 }
